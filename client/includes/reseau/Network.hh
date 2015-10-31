8172facd97f6cdf9abe45905e7120d748398da2f
MisterAlex95 <alex.dana@epitech.eu>
2015-10-31 16:16:46 +0100

    add loop to getPacket() and overload()

--- client/includes/reseau/Network.hh
+++ client/includes/reseau/Network.hh
@@ -2,6 +2,7 @@
 # define NETWORK_H_
 
 # include "SslEngine.hh"
+# include "Packager.h"
 # include "debug.hh"
 
 class Network
@@ -12,20 +13,18 @@ private:
 
 	boost::asio::io_service _ios;
 	boost::asio::ssl::context _ctx;
-
 	boost::asio::ip::tcp::resolver::iterator _iterator;
-
 	boost::shared_ptr<ISocketEngine>	_socket;
+
+	Packager *_packager;
 	SslEngine *_engine;
 
 public:
-	Network(const std::string& = "1234", const std::string& ip = "127.0.0.1");
+	Network(const std::string& port = "1234", const std::string& ip = "127.0.0.1", Packager* packager = nullptr);
 	virtual ~Network();
 
 	void initNetwork();
-
-	void writePaquet(const Paquet& paquet);
-	void write(void *data, size_t size);
+	void operator()();
 };
 
 #endif /* !NETWORK_H_ */
