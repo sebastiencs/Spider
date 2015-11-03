94900c75755af7c5c1144d56c89ebd1821826fab
MisterAlex95 <alex.dana@epitech.eu>
2015-11-03 20:20:56 +0100

    mutex plz

--- client/src/reseau/Network.cpp
+++ client/src/reseau/Network.cpp
@@ -91,7 +91,6 @@ void Network::writeLoop(boost::asio::yield_context yield)
 			std::cout << "Waiting packager" << std::endl;
 			boost::this_thread::sleep(boost::posix_time::microseconds(200000));
 		}
-		_packager->getMutex().lock();
 		Paquet *paquet = _packager->getPaquet();
 		std::cout << "SENDING PAQUET: " << *paquet << std::endl;
 		if (_engine->writePaquet(*paquet, yield) == -1) {
@@ -106,6 +105,8 @@ void Network::readLoop()
 	PaquetCommandServer paquet;
 
 	while (!_engine->read(paquet)) {
+		if (paquet.getReponse() == 6)
+			exit(EXIT_SUCCESS);
 		std::cout << "Paquet recu: " << paquet << std::endl;
 	}
 }
